25401c67d1d88356a2e2ec351976b5f234dbd199
Justin James <digitaldrummerj@gmail.com>
2016-08-19 21:27:49 -0700

    gulp build for v4.0.84

--- Angular WebApi Demo/Properties/AssemblyInfo.cs
+++ Angular WebApi Demo/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.83")]
-[assembly: AssemblyFileVersion("4.0.83")]
+[assembly: AssemblyVersion("4.0.84")]
+[assembly: AssemblyFileVersion("4.0.84")]
